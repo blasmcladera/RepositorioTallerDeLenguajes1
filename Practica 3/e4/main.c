800005122c4641a643ad92c3bc6f544b8fbab692
blasmcladera <blascladera715@gmail.com>
2024-04-12 08:49:42 -0300

    Termino e4.

--- Practica 3/e4/main.c
+++ Practica 3/e4/main.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct carta{
-    char palo[10];
+#include <time.h>
+
+typedef struct carta_{
+    char *palo;
     int numero;
 } TCarta;
 
 void inicializarMazo(TCarta[]);
+void printMazo(TCarta[]);
+void barajarMazo(TCarta[]);
+TCarta tomarCarta(TCarta[],int);
+void printCarta(TCarta);
+
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    srand(time(NULL));
+    TCarta mazo[50],carta;
+    inicializarMazo(mazo);
+    printMazo(mazo);
+    printf("----------------------------\n");
+    barajarMazo(mazo);
+    printMazo(mazo);
+    printf("Ingrese numero de carta: \n");
+    scanf("%d",&n);
+    carta=tomarCarta(mazo,n);
+    printCarta(carta);
     return 0;
 }
 
@@ -16,7 +34,7 @@ void inicializarMazo(TCarta m[]){
     int i=0;
     for(;i<12;i++){
         m[i].palo="Espadas";
-        m[i].numero=i;
+        m[i].numero=i+1;
     }
     for(;i<24;i++){
         m[i].palo="Oro";
@@ -34,4 +52,28 @@ void inicializarMazo(TCarta m[]){
         m[i].palo="Comodin";
         m[i].numero=-1;
     }
+}
+
+void printMazo(TCarta m[]){
+    int i=0;
+    for (;i<50;i++)printf("Palo: %7s, numero: %2d\n",m[i].palo,m[i].numero);
+}
+
+void barajarMazo (TCarta m[]){
+    int r,i;
+    TCarta aux;
+    for (i=0;i<50;i++){
+        r=(rand()%50);
+        aux=m[r];
+        m[r]=m[i];
+        m[i]=aux;
+    }
+}
+
+TCarta tomarCarta (TCarta m[],int n){
+    return m[n-1];
+}
+
+void printCarta(TCarta c){
+    printf("Palo: %7s, numero: %2d\n",c.palo,c.numero);
 }
